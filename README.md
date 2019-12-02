53b66ff0061f3e3d2d38fe706c8c0fce441e8492
Mikhalcha Vitaly <vetmih@mail.ru>
2019-12-02 15:35:42 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# data visualization via API" 
---
+++ python_repos.py
@@ -0,0 +1,47 @@
+import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
+
+# создание вызова API и сохранение ответа.
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+r = requests.get(url)
+print('status code:', r.status_code)
+
+# сохранение ответа API в переменной
+response_dict = r.json()
+print('Total repositories:', response_dict['total_count'])
+
+# Анализ информации о репозиториях.
+repo_dicts = response_dict['items']
+
+names, plot_dicts = [], []
+for repo_dict in repo_dicts:
+    names.append(repo_dict['name'])
+# Получаем описание проекта если оно имеется.
+    description = repo_dict['description']
+    if not description:
+        description = 'No description provided'
+    plot_dict = {
+        'value': repo_dict['stargazers_count'],
+        'label': description,
+        'xlink': repo_dict['html_url'],  # добавление активных ссылок на диаграмму
+        }
+    plot_dicts.append(plot_dict)
+
+# Построение визуализации
+my_style = LS('#333322', base_style=LCS)
+my_style.title_font_size = 24
+my_style.label_font_size = 14
+my_style.major_label_font_size = 18
+my_config = pygal.Config()
+my_config.x_label_rotation = 45
+my_config.show_legend = False
+my_config.truncate_label = 15
+my_config.show_y_guides = False
+my_config.width = 1000
+
+chart = pygal.Bar(my_config, style=my_style)
+chart.title = 'Most-Starred Python projects on GitHub'
+chart.x_labels = names
+chart.add('', plot_dicts)
+chart.render_to_file('python_repos.svg')
